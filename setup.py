ec2608b10285130e620c2549c02a0b4dd5ba84e8
jiaxin <edison7500@gmail.com>
2019-08-01 15:30:36 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
 import os
+from itertools import chain
+from glob import glob
 
 from setuptools import setup, find_packages
 
 import blocktech_cookielaw
+
+package_data_globs = (
+    'blocktech_cookielaw/templates/cookielaw/*.html',
+    'blocktech_cookielaw/static/cookielaw/*/*',
+)
+
+package_data = []
+for f in chain(*map(glob, package_data_globs)):
+    package_data.append(f.split('/', 1)[1])
 
 setup(
     author='jiaxin',
@@ -10,7 +21,7 @@ setup(
     name='django-bt-cookielaw',
     version='.'.join(str(v) for v in blocktech_cookielaw.VERSION),
     description='Helps your Django project comply with EU cookie law regulations',
-    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
     url='https://github.com/edison7500/django-bt-cookielaw',
     license='GUN License',
     platforms=['OS Independent'],
@@ -20,7 +31,7 @@ setup(
         'django-classy-tags>=0.3.0',
     ],
     packages=find_packages(),
-    # package_data={'cookielaw': package_data},
+    package_data={'blocktech_cookielaw': package_data},
     include_package_data=False,
     zip_safe=False,
     # test_suite='tests',
