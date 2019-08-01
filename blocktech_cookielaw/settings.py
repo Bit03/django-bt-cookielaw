4a0e8c91d656d5cfabb8d74ce3e5e0d4b0dcde43
jiaxin <edison7500@gmail.com>
2019-08-01 11:43:50 +0800

    init blocktech cookielaw

---
+++ blocktech_cookielaw/settings.py
@@ -0,0 +1,7 @@
+from django.conf import settings
+
+
+_default_js = ('blocktech_cookielaw/js/cookielaw.js',)
+
+BLOCKTECH_COOKIELAW_JS = getattr(settings, 'BLOCKTECH_COOKIELAW_JS', _default_js)
+BLOCKTECH_COOKIELAW_CSS = getattr(settings, 'BLOCKTECH_COOKIELAW_CSS', {})
