3b7580b4ddb03a60682d860c192580ccd0fee3df
jiaxin <edison7500@gmail.com>
2019-08-01 14:11:39 +0800

    mereg

--- blocktech_cookielaw/tamplatetags/cookielaw_tags.py
+++ blocktech_cookielaw/tamplatetags/cookielaw_tags.py
@@ -5,13 +5,14 @@ from django.template.loader import render_to_string
 register = template.Library()
 
 
-class CookielawBanner(InclusionTag):
+class CookieLawBanner(InclusionTag):
     template = "blocktech_cookielaw/banner.html"
 
     def render_tag(self, context, **kwargs):
         template_filename = self.get_template(context, **kwargs)
+        data = self.get_context(context, **kwargs)
 
         return render_to_string(template_filename, data, context.request)
 
 
-register.tag(CookielawBanner)
+register.tag(CookieLawBanner)
